df8bf80770c4a29812e05abfd3ec55b0c13e3f5b
leonadup <leona.d@icloud.com>
2025-05-14 01:00:28 +0200

    changement de fond et centre le texte

--- fonctionnalites.js
+++ fonctionnalites.js
@@ -998,3 +998,49 @@ document.addEventListener('DOMContentLoaded', function() {
         translatePage(this.value);
     });
 });
+
+/* AJOUT : configuration Particles.js */
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS("particles-js", {
+      particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: "#00ffff" },
+        shape: {
+          type: "circle",
+          stroke: { width: 0, color: "#000000" },
+          polygon: { nb_sides: 5 }
+        },
+        opacity: { value: 0.5, random: false },
+        size: { value: 3, random: true },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#00ffff",
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 3,
+          direction: "none",
+          random: false,
+          straight: false,
+          bounce: true
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: { enable: true, mode: "grab" },
+          onclick: { enable: true, mode: "push" },
+          resize: true
+        },
+        modes: {
+          grab: { distance: 140, line_linked: { opacity: 1 } },
+          push: { particles_nb: 4 }
+        }
+      },
+      retina_detect: true
+    });
+  });
+  
